dd170259b97353c99b442b3265ba2c0d22f16aa5
Klim <114877716+TiKlim@users.noreply.github.com>
2024-02-17 10:23:40 +0300

    Add files via upload

---
+++ KlimClicker/app/src/main/java/com/example/klimclicker/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.klimclicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private long score = 0;
+    int score = 0;
+    int money = 1;
+    TextView mainTextView;
+    Button mainBtn;
+    Button mainBtn2;
+    Button mainBtn3;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainBtn = (Button) findViewById(R.id.main_btn);
+        mainBtn2 = (Button) findViewById(R.id.main_btn_2);
+        mainBtn3 = (Button) findViewById(R.id.main_btn_3);
+        mainBtn();//*
+        mainBtn2();//*
+        mainBtn3();
+    }
+    void mainBtn(){
+        mainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                score += money;
+                mainTextView.setText("Кликов: " + score);
+            }
+        });
+    }
+    void mainBtn2(){
+        mainBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                score -= money;
+                mainTextView.setText("Кликов: " + score);
+            }
+        });
+    }
+    void mainBtn3(){
+        mainBtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                score = 0;
+                mainTextView.setText("Кликов: " + score);
+            }
+        });
+    }
+}
