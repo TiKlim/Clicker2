793b5957e6967b2693f87c095e9c15c7467d06f9
Klim <karelian_company@mail.ru>
2024-03-16 16:00:20 +0300

    16.03.2024/15:57

--- KlimClicker/app/src/main/java/com/example/klimclicker/MainActivity.java
+++ KlimClicker/app/src/main/java/com/example/klimclicker/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Button;
 import android.os.Bundle;
@@ -14,18 +15,18 @@ public class MainActivity extends AppCompatActivity {
     int score = 0;
     int money = 1;
     TextView mainTextView;
-    Button mainBtn;
-    Button mainBtn2;
-    Button mainBtn3;
+    ImageButton mainBtn;
+    ImageButton mainBtn2;
+    ImageButton mainBtn3;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mainTextView = (TextView) findViewById(R.id.main_textview);
-        mainBtn = (Button) findViewById(R.id.main_btn);
-        mainBtn2 = (Button) findViewById(R.id.main_btn_2);
-        mainBtn3 = (Button) findViewById(R.id.main_btn_3);
+        mainBtn = (ImageButton) findViewById(R.id.main_btn);
+        mainBtn2 = (ImageButton) findViewById(R.id.main_btn_2);
+        mainBtn3 = (ImageButton) findViewById(R.id.main_btn_3);
         mainBtn();//*
         mainBtn2();//*
         mainBtn3();
